f718f3ccdbb965a131d83f877fe4bdcfa57483ea
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2014-12-16 23:29:59 +0100

    Test if build ok before deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 sudo docker run -v $GOPATH/src:/src mobile /bin/bash -c 'cd /src/github.com/tbruyelle/snake && ./make.bash'
-adb uninstall com.kamosoft.snake
-adb install bin/nativeactivity-debug.apk
+if [[ $? -eq 0  ]] 
+then
+	adb uninstall com.kamosoft.snake
+	adb install bin/nativeactivity-debug.apk
+fi
