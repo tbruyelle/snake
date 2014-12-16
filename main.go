7260847605b4ff12800198739d7c71f4c5f1b511
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2014-12-16 23:30:21 +0100

    start play with sprite

--- main.go
+++ main.go
@@ -2,31 +2,33 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// An app that draws a green triangle on a red background.
 package main
 
 import (
-	"encoding/binary"
+	"image"
 	"log"
+	"math"
+	"os"
+	"time"
+
+	_ "image/jpeg"
 
 	"golang.org/x/mobile/app"
 	"golang.org/x/mobile/app/debug"
 	"golang.org/x/mobile/event"
 	"golang.org/x/mobile/f32"
-	"golang.org/x/mobile/geom"
 	"golang.org/x/mobile/gl"
-	"golang.org/x/mobile/gl/glutil"
+	"golang.org/x/mobile/sprite"
+	"golang.org/x/mobile/sprite/clock"
+	"golang.org/x/mobile/sprite/glsprite"
 )
 
 var (
-	program  gl.Program
-	position gl.Attrib
-	offset   gl.Uniform
-	color    gl.Uniform
-	buf      gl.Buffer
+	start     = time.Now()
+	lastClock = clock.Time(-1)
 
-	green    float32
-	touchLoc geom.Point
+	eng   = glsprite.Engine()
+	scene *sprite.Node
 )
 
 func main() {
@@ -36,82 +38,115 @@ func main() {
 	})
 }
 
-// TODO(crawshaw): Need an easier way to do GL-dependent initialization.
+func draw() {
+	if scene == nil {
+		loadScene()
+	}
 
-func initGL() {
-	var err error
-	program, err = glutil.CreateProgram(vertexShader, fragmentShader)
-	if err != nil {
-		log.Printf("error creating GL program: %v", err)
+	now := clock.Time(time.Since(start) * 60 / time.Second)
+	if now == lastClock {
+		// TODO: figure out how to limit draw callbacks to 60Hz instead of
+		// burning the CPU as fast as possible.
+		// TODO: (relatedly??) sync to vblank?
 		return
 	}
+	lastClock = now
 
-	buf = gl.GenBuffer()
-	gl.BindBuffer(gl.ARRAY_BUFFER, buf)
-	gl.BufferData(gl.ARRAY_BUFFER, gl.STATIC_DRAW, triangleData)
-
-	position = gl.GetAttribLocation(program, "position")
-	color = gl.GetUniformLocation(program, "color")
-	offset = gl.GetUniformLocation(program, "offset")
-	touchLoc = geom.Point{geom.Width / 2, geom.Height / 2}
-}
-
-func touch(t event.Touch) {
-	touchLoc = t.Loc
-}
-
-func draw() {
-	if program.Value == 0 {
-		initGL()
-	}
-
-	gl.ClearColor(1, 0, 0, 1)
+	gl.ClearColor(1, 1, 1, 1)
 	gl.Clear(gl.COLOR_BUFFER_BIT)
-
-	gl.UseProgram(program)
-
-	green += 0.01
-	if green > 1 {
-		green = 0
-	}
-	gl.Uniform4f(color, 0, green, 0, 1)
-
-	gl.Uniform2f(offset, float32(touchLoc.X/geom.Width), float32(touchLoc.Y/geom.Height))
-
-	gl.BindBuffer(gl.ARRAY_BUFFER, buf)
-	gl.EnableVertexAttribArray(position)
-	gl.VertexAttribPointer(position, coordsPerVertex, gl.FLOAT, false, 0, 0)
-	gl.DrawArrays(gl.TRIANGLES, 0, vertexCount)
-	gl.DisableVertexAttribArray(position)
-
+	eng.Render(scene, now)
 	debug.DrawFPS()
 }
 
-var triangleData = f32.Bytes(binary.LittleEndian,
-	0.0, 0.4, 0.0, // top left
-	0.0, 0.0, 0.0, // bottom left
-	0.4, 0.0, 0.0, // bottom right
-)
+func touch(t event.Touch) {
+}
+
+func newNode() *sprite.Node {
+	n := &sprite.Node{}
+	eng.Register(n)
+	scene.AppendChild(n)
+	return n
+}
+
+func loadScene() {
+	texs := loadTextures()
+	scene = &sprite.Node{}
+	eng.Register(scene)
+	eng.SetTransform(scene, f32.Affine{
+		{1, 0, 0},
+		{0, 1, 0},
+	})
+
+	var n *sprite.Node
+
+	n = newNode()
+	eng.SetSubTex(n, texs[texBooks])
+	eng.SetTransform(n, f32.Affine{
+		{36, 0, 0},
+		{0, 36, 0},
+	})
+
+	n = newNode()
+	eng.SetSubTex(n, texs[texFire])
+	eng.SetTransform(n, f32.Affine{
+		{72, 0, 144},
+		{0, 72, 144},
+	})
+
+	n = newNode()
+	n.Arranger = arrangerFunc(func(eng sprite.Engine, n *sprite.Node, t clock.Time) {
+		// TODO: use a tweening library instead of manually arranging.
+		t0 := uint32(t) % 120
+		if t0 < 60 {
+			eng.SetSubTex(n, texs[texGopherR])
+		} else {
+			eng.SetSubTex(n, texs[texGopherL])
+		}
+
+		u := float32(t0) / 120
+		u = (1 - f32.Cos(u*2*math.Pi)) / 2
+
+		tx := 18 + u*48
+		ty := 36 + u*108
+		sx := 36 + u*36
+		sy := 36 + u*36
+		eng.SetTransform(n, f32.Affine{
+			{sx, 0, tx},
+			{0, sy, ty},
+		})
+	})
+}
 
 const (
-	coordsPerVertex = 3
-	vertexCount     = 3
+	texBooks = iota
+	texFire
+	texGopherR
+	texGopherL
 )
 
-const vertexShader = `#version 100
-uniform vec2 offset;
+func loadTextures() []sprite.SubTex {
+	f, err := os.Open("waza-gophers.jpeg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	img, _, err := image.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t, err := eng.LoadTexture(img)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-attribute vec4 position;
-void main() {
-	// offset comes in with x/y values between 0 and 1.
-	// position bounds are -1 to 1.
-	vec4 offset4 = vec4(2.0*offset.x-1.0, 1.0-2.0*offset.y, 0, 0);
-	gl_Position = position + offset4;
-}`
+	return []sprite.SubTex{
+		texBooks:   sprite.SubTex{t, image.Rect(4, 71, 132, 182)},
+		texFire:    sprite.SubTex{t, image.Rect(330, 56, 440, 155)},
+		texGopherR: sprite.SubTex{t, image.Rect(152, 10, 152+140, 10+90)},
+		texGopherL: sprite.SubTex{t, image.Rect(162, 120, 162+140, 120+90)},
+	}
+}
 
-const fragmentShader = `#version 100
-precision mediump float;
-uniform vec4 color;
-void main() {
-	gl_FragColor = color;
-}`
+type arrangerFunc func(e sprite.Engine, n *sprite.Node, t clock.Time)
+
+func (a arrangerFunc) Arrange(e sprite.Engine, n *sprite.Node, t clock.Time) { a(e, n, t) }
